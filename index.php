d027e0747650d165eb91b715d7558546b9b51a2f
Amanda Sjölander <amandasjolander@live.se>
2015-06-08 14:25:28 +0200

    Firstcommit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Title</title>
+	<meta name="format-detection" content="telephone=no">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width">
+	<meta charset="UTF-8">
+	
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" media="all">
+	<link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+
+	<script src="js/jquery.js"></script>
+	<script src="js/script.js"></script>
+</head>
+
+<body class="clearfix">
+
+
+	<div id="tridiv">
+	  <div class="scene" style="-webkit-transform:rotateX(-19deg) rotateY(63deg); -moz-transform:rotateX(-19deg) rotateY(63deg); -ms-transform:rotateX(-19deg) rotateY(63deg); transform:rotateX(-19deg) rotateY(63deg); ">
+	    <div class="shape cuboid-1 cub-1">
+	      <div class="face ft">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face bk">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face rt">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face lt">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face bm">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face tp">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 1);"></div>
+	      </div>
+	    </div>
+	    <div class="shape cuboid-2 cub-2">
+	      <div class="face ft">
+	        <div class="photon-shader" style="background-color: rgba(79,22,54, 0.172549);"></div>
+	      </div>
+	      <div class="face bk">
+	        <div class="photon-shader" style="background-color: rgba(79,22,54, 0.172549);"></div>
+	      </div>
+	      <div class="face rt">
+	        <div class="photon-shader" style="background-color: rgba(79,22,54, 0.172549);"></div>
+	      </div>
+	      <div class="face lt">
+	        <div class="photon-shader" style="background-color: rgba(79,22,54, 0.172549);"></div>
+	      </div>
+	      <div class="face bm">
+	        <div class="photon-shader" style="background-color: rgba(79,22,54, 0.172549);"></div>
+	      </div>
+	      <div class="face tp">
+	        <div class="photon-shader" style="background-color: rgba(79,22,54, 0.172549);"></div>
+	      </div>
+	    </div>
+	    <div class="shape cuboid-3 cub-3">
+	      <div class="face ft">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face bk">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face rt">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face lt">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face bm">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	      <div class="face tp">
+	        <div class="photon-shader" style="background-color: rgba(0, 0, 0, 0.172549);"></div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+</body>
+</html>
---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(window).load(function() {
+
+  $(".cub-3 .tp").on('click', function(){
+    $(".cub-2").toggleClass("active");
+  });
+
+  
+
+});
+
+$(document).ready(function() {
+
+});
+
