d3edb31ab4731e491d47ca8f05f1d015ea5133bf
Amanda Sjölander <amandasjolander@live.se>
2015-06-11 15:30:15 +0200

    query click sound :)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
 $(window).load(function() {
+});
+
+$(document).ready(function(){
+
+  var audioElementOn = document.createElement('audio');
+  audioElementOn.setAttribute('src', 'soundeffects/click.mp3');
+  audioElementOn.load();
+
+  var audioElementOff = document.createElement('audio');
+  audioElementOff.setAttribute('src', 'soundeffects/click.mp3');
+  audioElementOff.load();
+
 
   $(".switch").on('click', function(){
+    audioElementOn.play();
     $(".lid").addClass("active");
     $(".cyl-3").addClass("active");
     $(".cyl-4").addClass("active");
     $(".cyl-5").addClass("active");
     $(".cyl-6").addClass("active");
     $(".switch").addClass("active");
+    
 
     $(".cyl-6").one('transitionend webkitTransitionEnd', function(){
+      audioElementOff.play();
       $(".cyl-3").removeClass("active");
       $(".cyl-4").removeClass("active");
       $(".cyl-5").removeClass("active");
@@ -17,12 +32,11 @@ $(window).load(function() {
       $(".lid").removeClass("active");
     });   
   });
- 
 
-});
 
-$(document).ready(function(){
+
   var socket = io('http://10.0.1.112:5000');
+
   socket.on('clientData', function(data){
     $('#activeUsers').html(data.clients.active);
   });
